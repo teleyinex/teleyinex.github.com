452a29b5389be03f0b9744432946f7f73b1786b1
Daniel Lombraña González <teleyinex@gmail.com>
2016-09-06 15:59:04 +0200

    Minimize.

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -19,14 +19,14 @@ module.exports = {
         },
         ]
     },
-    //plugins: [
-    //    new webpack.optimize.UglifyJsPlugin({
-    //        compress: {
-    //            warnings: false,
-    //        },
-    //        output: {
-    //            comments: false,
-    //        },
-    //    }),
-    //]
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+            output: {
+                comments: false,
+            },
+        }),
+    ]
 }
