83021918cba327110b6d48fd1a73d93ce84d55f4
Daniel Lombraña González <teleyinex@gmail.com>
2016-09-07 10:45:44 +0200

    Add footer with JS.

--- js/app.js
+++ js/app.js
@@ -2,11 +2,10 @@ import loading from './loading.js';
 import menu from './menu.js';
 import closing from './closing.js';
 import photography from './photos.js';
+import footer from './footer.js';
 
 window.onload = function() {
     loading.heart.animate({height: 1},  500, mina.easein);
     window.setTimeout(loading.trigger, 500);
 
 }
-
-
