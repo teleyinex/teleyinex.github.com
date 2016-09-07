bdeb258ffbed57cb1c73bed629ff158b3bee612b
Daniel Lombraña González <teleyinex@gmail.com>
2016-09-07 10:53:52 +0200

    Remove comments.

--- js/footer.js
+++ js/footer.js
@@ -1,37 +1,31 @@
 var footer; 
 
 if ($(".navbar-about").length) {
-    console.log($(".navbar-about").length);
     footer = $("<footer/>", {'class': 'footer about'});
     $("body").append(footer);
 }
 
 if ($(".navbar-blog").length) {
-    console.log($(".navbar-blog").length);
     footer = $("<footer/>", {'class': 'footer blog'});
     $("body").append(footer);
 }
 
 if ($(".navbar-blogpost").length) {
-    console.log($(".navbar-blogpost").length);
     footer = $("<footer/>", {'class': 'footer blogpost'});
     $("body").append(footer);
 }
 
 if ($(".navbar-project").length) {
-    console.log($(".navbar-project").length);
     footer = $("<footer/>", {'class': 'footer project'});
     $("body").append(footer);
 }
 
 if ($(".navbar-project-info").length) {
-    console.log($(".navbar-project-info").length);
     footer = $("<footer/>", {'class': 'footer project-info'});
     $("body").append(footer);
 }
 
 if ($(".navbar-photography").length) {
-    console.log($(".navbar-photography").length);
     footer = $("<footer/>", {'class': 'footer photography'});
     $("body").append(footer);
 }
