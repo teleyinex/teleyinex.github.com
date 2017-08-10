c3884148f2de57a29cf46f056f780db9af4907ac
Daniel Lombraña González <teleyinex@gmail.com>
2017-08-10 14:44:55 +0200

    Add save.

--- pinboard/components/App.vue
+++ pinboard/components/App.vue
@@ -1,55 +1,133 @@
 <template>
-        <div class="container">
-            <div class="columns is-centered">
-                <div class="column is-5">
-                    <div v-for="link in links" class="card">
-                        <header class="card-header">
-                            <p class="card-header-title">
-                                <a class="is-size-5" :href="link.u">{{shortTitle(link.d)}}</a>
-                            </p>
-                            <a class="card-header-icon">
-                                <span class="icon">
-                                    <b-dropdown>
-                                        <span  slot="trigger">
-                                            <b-icon icon="more_vert"></b-icon>
+    <div class="container">
+        <div class="columns is-centered">
+            <div class="column is-5">
+                <b-tabs position="is-centered">
+                    <b-tab-item label="Popular">
+                        <div style="padding:5px;">
+                            <div v-for="link in links" class="card">
+                                <header class="card-header">
+                                    <p class="card-header-title">
+                                    <a class="is-size-5" :href="link.u">{{shortTitle(link.d)}}</a>
+                                    </p>
+                                    <a class="card-header-icon">
+                                        <span class="icon">
+                                            <b-dropdown>
+                                                <span  slot="trigger">
+                                                    <b-icon icon="more_vert"></b-icon>
+                                                </span>
+                                                <b-dropdown-option><a target="_blank" :href="ssnn('twitter', link)">Share on Twitter</a></b-dropdown-option>
+                                                <b-dropdown-option><a target="_blank" :href="ssnn('facebook', link)">Share on Facebook</a></b-dropdown-option>
+                                            </b-dropdown>
                                         </span>
-                                        <b-dropdown-option><a target="_blank" :href="ssnn('twitter', link)">Share on Twitter</a></b-dropdown-option>
-                                        <b-dropdown-option><a target="_blank" :href="ssnn('facebook', link)">Share on Facebook</a></b-dropdown-option>
-                                    </b-dropdown>
-                                </span>
-                            </a>
-                        </header>
-                        <div class="card-content">
-                            <div class="media">
-                                <div class="media-content">
-                                    <p class="title is-6">{{link.n}}</p>
+                                    </a>
+                                </header>
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <p class="title is-6">{{link.n}}</p>
+                                        </div>
+                                    </div>
+                                    <div class="content">
+                                        <p class="subtitle is-6">Pin saved by: {{link.a}} on {{link.dt}}</p>
+                                        <p v-if="tags(link.t)">
+                                        <span class="has-text-primary" v-for="tag in link.t">#{{tag}} </span> 
+                                        </p>
+                                    </div>
                                 </div>
+                                <footer class="card-footer">
+                                    <p class="card-footer-item">
+                                    <span>
+                                        <a :href="link.u">View pin</a>
+                                    </span>
+                                    </p>
+                                    <p v-if="notSaved(link)" @click="add(link)" class="card-footer-item">
+                                        <i class="material-icons has-text-primary">favorite_border</i>
+                                    </p>
+                                    <p v-else @click="remove(link)" class="card-footer-item">
+                                        <i class="material-icons has-text-primary">favorite</i>
+                                    </p>
+                                </footer>
                             </div>
-                            <div class="content">
-                                <p class="subtitle is-6">Pin saved by: {{link.a}} on {{link.dt}}</p>
-                                <p v-if="tags(link.t)">
-                                    <span class="has-text-primary" v-for="tag in link.t">#{{tag}} </span> 
-                                </p>
+                            <infinite-loading :on-infinite="onInfinite" ref="infiniteLoading"></infinite-loading>
+                        </div>
+                    </b-tab-item>
+
+                    <b-tab-item label="Hearted">
+                        <div style="padding:5px;">
+                            <div v-for="link in savedLinks" class="card">
+                                <header class="card-header">
+                                    <p class="card-header-title">
+                                    <a class="is-size-5" :href="link.u">{{shortTitle(link.d)}}</a>
+                                    </p>
+                                    <a class="card-header-icon">
+                                        <span class="icon">
+                                            <b-dropdown>
+                                                <span  slot="trigger">
+                                                    <b-icon icon="more_vert"></b-icon>
+                                                </span>
+                                                <b-dropdown-option><a target="_blank" :href="ssnn('twitter', link)">Share on Twitter</a></b-dropdown-option>
+                                                <b-dropdown-option><a target="_blank" :href="ssnn('facebook', link)">Share on Facebook</a></b-dropdown-option>
+                                            </b-dropdown>
+                                        </span>
+                                    </a>
+                                </header>
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <p class="title is-6">{{link.n}}</p>
+                                        </div>
+                                    </div>
+                                    <div class="content">
+                                        <p class="subtitle is-6">Pin saved by: {{link.a}} on {{link.dt}}</p>
+                                        <p v-if="tags(link.t)">
+                                        <span class="has-text-primary" v-for="tag in link.t">#{{tag}} </span> 
+                                        </p>
+                                    </div>
+                                </div>
+                                <footer class="card-footer">
+                                    <p class="card-footer-item">
+                                    <span>
+                                        <a :href="link.u">View pin</a>
+                                    </span>
+                                    </p>
+                                    <p @click="add(link)" v-if="notSaved(link)"  class="card-footer-item">
+                                        <i class="material-icons has-text-primary">favorite_border</i>
+                                    </p>
+                                    <p @click="remove(link)" v-else class="card-footer-item">
+                                        <i class="material-icons has-text-primary">favorite</i>
+                                    </p>
+                                </footer>
                             </div>
                         </div>
-                        <footer class="card-footer">
-                            <p class="card-footer-item">
-                            <span>
-                                <a :href="link.u">View pin</a>
-                            </span>
-                            </p>
-                        </footer>
-                    </div>
-                    <infinite-loading :on-infinite="onInfinite" ref="infiniteLoading"></infinite-loading>
-                </div>
+                    </b-tab-item>
+                </b-tabs>
             </div>
         </div>
+    </div>
 </template>
 <script>
 import axios from "axios"
 import share from "social-share"
 import ellipsis from "text-ellipsis"
 import InfiniteLoading from "vue-infinite-loading"
+import _ from 'lodash'
+
+// localStorage persistence
+var STORAGE_KEY = 'pinboard'
+var linkStorage = {
+  fetch: function () {
+    var links = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    //links.forEach(function (link, index) {
+    //  link.id = index
+    //})
+    //linkStorage.uid = links.length
+    return links
+  },
+  save: function (links) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(links))
+  }
+}
 
 export default {
     components: {
@@ -59,14 +137,25 @@ export default {
         return {
             links: [],
             offset: 0,
-            limit: 5
-            
+            limit: 5,
+            savedLinks: linkStorage.fetch()
         }
     },
     created(){
         this.links = window.pins.slice(this.offset, this.limit - 1)
     },
     methods: {
+        notSaved(link){
+            var found = _.find(this.savedLinks, {'u': link.u})
+            if (found === undefined) return true
+            else return false
+        },
+        add(link){
+            this.savedLinks.push(link)
+        },
+        remove(link){
+            this.savedLinks.splice(this.savedLinks.indexOf(link), 1)
+        },
         tags(tags){
             if ((tags.length > 0) && (tags[0] !== '')) {
                 return true
@@ -75,11 +164,11 @@ export default {
         },
         meta(link){
             Metascraper
-              .scrapeUrl(link)
-              .then((metadata) => {
-                console.log(metadata)  
-              })
-        
+                .scrapeUrl(link)
+                .then((metadata) => {
+                    console.log(metadata)  
+                })
+
         },
         ssnn(network, link) {
             return share(network, {title: link.d, url: link.u})
@@ -87,13 +176,21 @@ export default {
         shortTitle(text){
             return ellipsis(text, 50)
         },
- onInfinite() {
-      setTimeout(() => {
-          this.offset = this.offset + this.limit
-          this.links = this.links.concat(window.pins.slice(this.offset, this.offset + this.limit -1))
-          this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded');
-      }, 1000);
+        onInfinite() {
+            setTimeout(() => {
+                this.offset = this.offset + this.limit
+                this.links = this.links.concat(window.pins.slice(this.offset, this.offset + this.limit -1))
+                this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded');
+            }, 1000);
+        },
     },
+    watch: {
+        savedLinks: {
+              handler: function (savedLinks) {
+                linkStorage.save(savedLinks)
+              },
+              deep: true
+            }
     }
 }
 </script>
